b683b4e6a7d0ead21c4db76fb029dba950cea45a
Bryan <bmvingeniero@gmail.com>
2021-09-17 00:27:55 -0500

    develop: Se avanza hasta la clase del Sabado 11 de Septiembre

--- Bryan/Grupo56/Aplicacion/Program.cs
+++ Bryan/Grupo56/Aplicacion/Program.cs
@@ -6,7 +6,7 @@ namespace Aplicacion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola mundo Cruel!");
         }
     }
 }
---
+++ Bryan/Grupo56/Persistencia/AplicacionContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Dominio;
+namespace Persistencia
+{
+    public class AplicacionContext: DbContext
+    {
+        public DbSet<Cliente> personas{get;set;}
+        private const string connectionString = @"Data Source=localhost\sqlexpress;Initial Catalog = Grupo56;Integrated Security = True";
+           protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            if(!optionsBuilder.IsConfigured){
+                optionsBuilder
+                .UseSqlServer(connectionString);
+            }
+        }
+    }
+}
