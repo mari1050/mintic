e0c839ec4ad1f64e72e231db4859f2f4ec75764f
Bryan <bmvingeniero@gmail.com>
2021-09-20 23:18:16 -0500

    main: Se carga informacion de clase empleado

---
+++ Bryan/Grupo56/Dominio/Empleado.cs
@@ -0,0 +1,11 @@
+namespace Dominio
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public int Tipo_documento { get; set; }
+        public int Documento { get; set; }
+    }
+}
