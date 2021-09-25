2f3167ad781895faecec2c21a21b4f4097fa5867
Bryan <bmvingeniero@gmail.com>
2021-09-25 13:55:37 -0500

    main: Se agrega la pagina personas al Frontend hecho con razor

---
+++ Bryan/Grupo56/Frontend/Pages/Personas/Personas.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Dominio;
+using Persistencia;
+
+
+namespace Frontend.Pages
+{
+public class PrivacyModel : PageModel
+    {
+       //private string[] saludos = { "Buenos dias", "Buenas tardes", "Buenas noches", "Hasta mañana" };
+       // public List<string> ListaSaludos { get; set; }
+       private static IRepositorioCliente repoCliente = new RepositorioCliente(new Persistencia.AplicacionContext());
+       public IEnumerable<Cliente> Clientecitos {get;set;}
+
+
+       
+        public void OnGet()
+        {
+           // ListaSaludos = new List<string>();
+           // ListaSaludos.AddRange(saludos);
+          Clientecitos=repoCliente.GetAllCliente();
+
+        }
+    }
+}
