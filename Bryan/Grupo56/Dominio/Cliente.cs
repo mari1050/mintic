0883769204769220e73cb7e9a0b875f19583191a
Bryan <bmvingeniero@gmail.com>
2021-10-02 14:26:23 -0500

    main: Clase 02/10/2021 actualizacion personal

--- Bryan/Grupo56/Dominio/Cliente.cs
+++ Bryan/Grupo56/Dominio/Cliente.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 namespace Dominio
 {
     public class Cliente
     {
         public int Id {get; set;}
+
+        //[Required(ErrorMessage = "El Nombre es requerido")]
         public string Nombre { get; set; }
         public int Edad { get; set; }
         public int Tipo_documento {get; set;}
--- Bryan/Grupo56/Frontend/Pages/Personas/Create.cshtml.cs
+++ Bryan/Grupo56/Frontend/Pages/Personas/Create.cshtml.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Dominio;
+using Persistencia;
 namespace MyApp.Namespace
 {
     public class CreateModel : PageModel
     {
+        public Cliente Persona { get; set; }
+        private static IRepositorioCliente repoCliente = new RepositorioCliente(new Persistencia.AplicacionContext());
+
         public void OnGet()
         {
         }
+        public void OnPost(Cliente persona)
+        {
+            repoCliente.AddCliente(persona);
+
+        }
     }
 }
+
+
